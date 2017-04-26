75afd0193f2da802b9638182bd6ca4e00cc8c57a
Raycao <raycao50@gmail.com>
2017-04-26 17:01:48 +0800

    更新筆記2017-04-26

---
+++ Java-Note/src/ch1/Integer.java
@@ -0,0 +1,10 @@
+package ch1;
+
+public class Integer {
+	public static void main(String args[]){
+		Integer number = 10;
+		System.out.println(number + 10);
+		System.out.println(number++);
+	}
+}
+
